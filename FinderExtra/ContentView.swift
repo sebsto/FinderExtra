712dcfb9a3456a1ce77ca63610e035164d176df2
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2020-08-31 22:21:29 +0200

    Initial Commit

---
+++ FinderExtra/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FinderExtra
+//
+//  Created by Stormacq, Sebastien on 31/08/2020.
+//  Copyright © 2020 Stormacq, Sebastien. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
